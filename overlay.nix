ff3e010131ad80f1a49a991dbe68a852b3b08e51
zimbatm <zimbatm@zimbatm.com>
2020-03-20 23:39:28 +0100

    make ghcide-nix composable
    When composing multiple niv sources, this allow to have a single
    top-level nix/sources.json as long as all the entries are compatible.

---
+++ overlay.nix
@@ -0,0 +1,44 @@
+_: pkgs:
+let
+  mkPackages = { ghc, stackYaml }:
+    pkgs.haskell-nix.stackProject {
+      src = pkgs.sources.ghcide;
+      inherit stackYaml;
+      modules = [
+        ({ config, ... }: {
+          ghc.package = ghc;
+          compiler.version = pkgs.lib.mkForce ghc.version;
+          reinstallableLibGhc = true;
+          packages.ghc.flags.ghci = pkgs.lib.mkForce true;
+          packages.ghci.flags.ghci = pkgs.lib.mkForce true;
+          # This fixes a performance issue, probably https://gitlab.haskell.org/ghc/ghc/issues/15524
+          packages.ghcide.configureFlags = [ "--enable-executable-dynamic" ];
+          packages.haskell-lsp.components.library.doHaddock = pkgs.lib.mkForce false;
+          packages.ghcide.components.library.doHaddock = pkgs.lib.mkForce false;
+          # Since GHC 8.8. Fix available but not released: https://github.com/blamario/monoid-subclasses/commit/2d3641af4b47fd448cc7c57940cb97c185cf0678
+          packages.monoid-subclasses.components.library.doHaddock = pkgs.lib.mkForce false;
+        })
+      ];
+    };
+
+  mkGhcide = args@{ ... }:
+    let
+      packages = mkPackages (
+        { ghc = pkgs.haskell-nix.compiler.ghc865; stackYaml = "stack.yaml"; }
+        // args);
+    in packages.ghcide.components.exes.ghcide // { inherit packages; };
+in
+{
+  ghcide-nix = {
+    ghcide-ghc882 = mkGhcide { ghc = pkgs.haskell-nix.compiler.ghc882; stackYaml = "stack88.yaml"; };
+    ghcide-ghc865 = mkGhcide { ghc = pkgs.haskell-nix.compiler.ghc865; stackYaml = "stack.yaml"; };
+    ghcide-ghc864 = mkGhcide { ghc = pkgs.haskell-nix.compiler.ghc864; stackYaml = "stack.yaml"; };
+    ghcide-ghc844 = mkGhcide { ghc = pkgs.haskell-nix.compiler.ghc844; stackYaml = "stack84.yaml"; };
+    hie-bios = (mkPackages { ghc = pkgs.haskell-nix.compiler.ghc865; stackYaml = "stack.yaml"; }).hie-bios.components.exes.hie-bios;
+    inherit mkGhcide;
+  };
+
+  devTools = {
+    inherit (import pkgs.sources.niv { inherit pkgs; }) niv;
+  };
+}
